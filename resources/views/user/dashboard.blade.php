35a1b470b2d4e652c24b5c7f04714614a26c7342
KgMyat-201 <kgmyat1846@gmail.com>
2022-06-19 17:48:57 +0630

    second commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -52,39 +52,57 @@
                         <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
                     </div>
                 </div>
-                <div class="card-body">
-                    <form action="">
-                        <div class="form-row" id="form_row">
-                            
-                                
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="Number" id="number" onkeyup="getRestrictNumber()" name="number">
-                            </div>
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="Amount" name="amount">
-                            </div>
-                            <button class="btn btn-outline-secondary">Enter</button>
-                          
+                <div class="card-body table-responsive" style="height: 400px;">
+						
+                    <table id="dynamic_field" class="table text-center"> 
+                    <!--<tr id="row'+iir+'" class="dynamic-added" style="height: 45px;"><td><input type="text" name="number[]" placeholder="Number" class="form-control name_list" required /></td><td colspan="2"><input type="text" name="amount[]" placeholder="Amount" class="form-control name_list1" required /></td><td><button type="button" name="remove" id="'+iir+'" class="btn btn-danger btn_remove">X</button></td></tr>-->
+                    <!--<td><button type="button" name="add" id="add" class="btn btn-success">Add</button></td>-->
+                    <?php 
+                        for ($xr = 1; $xr <51; $xr++) 
+                        {
+                            echo
+                            '<tr id="row'.$xr.'">   
+                                <td style="padding-top: 1rem;">
+                                '.$xr.'
+                                </td>
+                                <td><input type="text" name="number[]" placeholder="Number" class="form-control name_list"/></td>  
+                                <td colspan="2"><input type="text" name="amount[]" placeholder="Amount" class="form-control name_list1"/></td> 
+                                <td><button type="button" name="remove" id=	"'.$xr.'" class="btn btn-danger btn_remove">X</button></td>	
+                            </tr>';
+                        }
+                    ?>	
+                    <!--50-->
+                    </table>
+                </div>
+                <div class="card-footer text-center">
+                    <div class="row">
+
+                       
+                        <div class="ml-auto float-right">
+
+                            <input type="button" name="Cancel" id="Cancel" onclick="window.location.href='member.php'" class="btn btn-info" value="Cancel" /> 
+                            <input type="button"name="submit" id="submit" class="btn btn-info" value="Save" />  
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <script>
 
-        function getRestrictNumber() {
+        
+      </script>
+@endsection
+@section('script')
+<script>
+    function getRestrictNumber() {
           var number = document.getElementById('number').value
           console.log(number);
         // alert('hi');
         if(number==12){
             document.getElementById('number').value = null
         }
-        }
-      </script>
-@endsection
-@section('script')
-<script>
+    };
     $(function () {
       $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
@@ -92,6 +110,9 @@
           alwaysShowClose: false
         });
       });
-    })
+    });
+   
+	
+    
   </script>
 @endsection
